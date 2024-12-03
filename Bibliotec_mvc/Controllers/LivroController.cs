d1f80345bf8b611ee6bd40a2826cd44f2a361757
BEATRIZ DORINE YUSMY DA SILVA <40511347847@info.local>
2024-12-03 11:51:16 -0300

    Comecando um metodo de cadastrar

--- Bibliotec_mvc/Controllers/LivroController.cs
+++ Bibliotec_mvc/Controllers/LivroController.cs
@@ -29,14 +29,45 @@ namespace Bibliotec_mvc.Controllers
             List<Livro>listralivros = context.Livro.ToList();
 
             //Verificar se o livro tem reserva ou nao
-            var livroReservados = context.LivroReserva.ToDictionary(livro => livro.LivroID, livror => livror.DtReserva
-            livroReservados => livror.DtReservados );
+            var livroReservados = context.LivroReserva.ToDictionary(livro => livro.LivroID, livror => livror.DtReserva );
             
             ViewBag.Livros = listralivros;
             ViewBag.LivrosComReserva = livroReservados;
             
             
             return View();
+        }
+
+        [Route("Cadastro")]
+        //Metodo que retorna a tela de cadastro
+        public IActionResult Cadastro(){
+
+            ViewBag.Admin =  HttpContext.Session.GetString("Admin")!;
+
+            ViewBag.Categorias = context.Categoria.ToList();
+            //Retorna a View de cadastro:
+            return View();
+        }
+
+        //Metodo para cadastrar um livro:
+        [Route("Cadastrar")]
+        public IActionResult Cadastrar(IFormCollection form){
+           
+            Livro novoLivro = new Livro();
+
+            //O que meu usuario escrever no formulario sera atribuido ao novoLivro
+
+            novoLivro.Nome = form["Nome"].ToString();
+            novoLivro.Descricao = form["Descricao"].ToString();
+            novoLivro.Editora = form["Editora"].ToString();
+            novoLivro.Escritor = form["Escritor"].ToString();
+            novoLivro.Idioma = form["Idioma"].ToString();
+
+            //img
+
+            context.Livro.Add(novoLivro);
+
+            context.SaveChanges();
         }
 
         // [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
